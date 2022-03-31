5678cf85fc6fc286892a3a212dcf72f050dd36c4
MuhammadVikyArdiansyah <102362480+MuhammadVikyArdiansyah@users.noreply.github.com>
2022-03-31 19:03:31 +0700

    Update siapakahaku.js
    let poin

--- plugins/siapakahaku.js
+++ plugins/siapakahaku.js
@@ -1,7 +1,7 @@
 let fetch = require('node-fetch')
 
 let timeout = 120000
-let poin = 500
+let poin = 200
 let handler = async (m, { conn, usedPrefix }) => {
     conn.siapakahaku = conn.siapakahaku ? conn.siapakahaku : {}
     let id = m.chat
